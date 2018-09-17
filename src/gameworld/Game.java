3e28df1b96f5ddb2ad83d315d4d84d37d3e4c3d6
Annisha Akosah <akosahanni@ecs.vuw.ac.nz>
2018-09-18 10:42:49 +1200

    Added basic move functionality

--- src/gameworld/Game.java
+++ src/gameworld/Game.java
@@ -1,12 +1,62 @@
 package gameworld;
 
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Game {
-  public Game() {
-    System.out.println("Hello world.");
+	private static Player player;
 
-  }
+	public Game(){
+		// start the player in the centre of the room
+		this.player = new Player(new Point (5,5));
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public static void movePlayer(String direction) {
+		int dx = 0;
+		int dy = 0;
 
-  public static void main(String[] args) {
-    new Game();
-  }
+		switch(direction) {
+		case "w": 
+			dy = -1;
+			break;
+		case "a": 
+			dx = -1;
+			break;
+		case "s": 
+			dy = 1;
+			break;
+		case "d": 
+			dx = 1;
+			break;
+		}
+	
+		player.move(dx, dy);
+	}
+
+	private static String inputString(String msg) {
+		System.out.print(msg + " ");
+		while (true) {
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				return input.readLine();
+			} catch (IOException e) {
+				System.out.println("I/O Error ... please try again!");
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		new Game();
+
+		while(true) {
+			String dir = inputString("Direction: ");
+			movePlayer(dir);
+		}
+	}
 }
--- src/gameworld/Player.java
+++ src/gameworld/Player.java
@@ -1,4 +1,27 @@
 package gameworld;
 
+import java.awt.Point;
+
 public class Player {
+	private Point location;
+
+	
+	public Player(Point location) {
+		this.location = location;
+		
+	}
+
+	/**
+	 * Updates the player's current position on the board.
+	 * @param dx amount of steps moved in the x direction
+	 * @param dy amount of steps moved in the y direction
+	 */
+	public void move(int dx, int dy) {
+		location.translate(dx, dy);
+		System.out.println(location.toString());
+	}
+	
+	public Point getLocation() {
+		return location;
+	}
 }
