a47a2332d4ee5ce954ddb801817c986dde8e6963
Annisha Akosah <akosahanni@ecs.vuw.ac.nz>
2018-09-26 11:17:30 +1200

    added game options to menu bar

--- src/application/GUI.java
+++ src/application/GUI.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Optional;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -8,6 +11,7 @@ import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -24,17 +28,27 @@ public class GUI extends Application {
 
 
 	@Override public void start(Stage stage) {
-		
+
 		// create the menu bar
 		MenuBar menuBar = new MenuBar();
 		HBox hBox = new HBox(menuBar);
 		menuBar.setPrefWidth(WINDOW_WIDTH);
-	
-		// menu bar items
-		Menu newGame = new Menu("New Game");
-		menuBar.getMenus().add(newGame);
-		
-		Menu quitGame = new Menu("Quit");
+
+		// menu bar
+		Menu file = new Menu("File");
+		menuBar.getMenus().add(file);
+
+		// file 
+		MenuItem newGame = new MenuItem("New Game");
+		MenuItem editMap = new MenuItem("Edit Map");
+		MenuItem loadGame = new MenuItem("Load Game");
+		MenuItem saveGame = new MenuItem("Save Game");
+		file.getItems().addAll(newGame, editMap, loadGame, saveGame);
+
+		// quit
+		Label quit = new Label("Quit");
+		quit.setOnMouseClicked(mouseEvent->{ confirmExit(); });
+		Menu quitGame = new Menu("", quit);
 		menuBar.getMenus().add(quitGame);
 
 		// initialise the game panes
@@ -51,21 +65,21 @@ public class GUI extends Application {
 
 		HBox hb = new HBox();
 		hb.getChildren().add(stack);
-		
+
 		GridPane grid = new GridPane();
-		
+
 		grid.add(game, 0, 1);
 		grid.add(hb, 1, 1);
 		grid.add(hBox, 0, 0);
-		
+
 		setWindowRatio();
 
 		// Set the size of the window
 		grid.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT);		
-		
+
 		// Create the Scene
 		Scene scene = new Scene(grid);
-		
+
 		// Add the scene to the Stage
 		stage.setScene(scene);
 		// Set the title of the Stage
@@ -75,49 +89,49 @@ public class GUI extends Application {
 	}
 
 	public void setWindowRatio(){
-        //set ratios
-        this.game.setPrefSize(WINDOW_WIDTH*0.7, WINDOW_HEIGHT);
-        this.inventory.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.5);
-        this.options.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.1);
-        this.map.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.4);
-    }
+		//set ratios
+		this.game.setPrefSize(WINDOW_WIDTH*0.7, WINDOW_HEIGHT);
+		this.inventory.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.5);
+		this.options.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.1);
+		this.map.setPrefSize(WINDOW_WIDTH*0.3,WINDOW_HEIGHT*0.4);
+	}
 
 	public GridPane setGame() {
 		GridPane grid = new GridPane();
 		Text name = new Text("game");
 		grid.add(name, 0, 0);
 		grid.setStyle("-fx-background-color: red;");
-	
+
 		return grid;
 	}
 
 	public FlowPane setInventory() {
 		FlowPane flow = new FlowPane();
 		flow.setStyle("-fx-background-color: blue;");
-		
+
 		flow.setPadding(new Insets(8,1,1,8));
 		flow.setVgap(4);
 		flow.setHgap(4);
 		flow.setPrefWrapLength(WINDOW_WIDTH*0.15); // preferred width allows for two columns
 
-        ToggleButton btn;
-        ToggleGroup group = new ToggleGroup();
+		ToggleButton btn;
+		ToggleGroup group = new ToggleGroup();
 
-        String[] images = new String[]{"key.png","green-key.png", "red-key.png","unlit-bomb.png","two-coins.png","two-coins.png"};
+		String[] images = new String[]{"key.png","green-key.png", "red-key.png","unlit-bomb.png","two-coins.png","two-coins.png"};
 
-        for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < 6; i++) {
 
-            btn = new ToggleButton();
-            Image image = new Image(getClass().getResourceAsStream(images[i]));
-            ImageView imageView = new ImageView(image);
-            imageView.setFitHeight(80);
-            imageView.setFitWidth(80);
-            btn.setGraphic(imageView);
-            btn.setToggleGroup(group);
-            btn.setPrefSize(WINDOW_WIDTH*0.14, WINDOW_HEIGHT*0.5*0.23);
-            flow.getChildren().add(btn);
+			btn = new ToggleButton();
+			Image image = new Image(getClass().getResourceAsStream(images[i]));
+			ImageView imageView = new ImageView(image);
+			imageView.setFitHeight(80);
+			imageView.setFitWidth(80);
+			btn.setGraphic(imageView);
+			btn.setToggleGroup(group);
+			btn.setPrefSize(WINDOW_WIDTH*0.14, WINDOW_HEIGHT*0.5*0.23);
+			flow.getChildren().add(btn);
 
-        }
+		}
 
 		return flow;
 
@@ -127,8 +141,8 @@ public class GUI extends Application {
 		AnchorPane options = new AnchorPane();
 		Button pickupButton = new Button("Pick Up");
 		Button dropButton = new Button("Drop");
-        Button diffuseButton = new Button("Diffuse");
-        Button unlockButton = new Button("Unlock");
+		Button diffuseButton = new Button("Diffuse");
+		Button unlockButton = new Button("Unlock");
 
 		HBox hb = new HBox();
 		hb.setPadding(new Insets( 20,0,20,20));
@@ -147,6 +161,20 @@ public class GUI extends Application {
 		grid.setStyle("-fx-background-color: orange;");
 		grid.setPrefWidth(170);
 		return grid;
+	}
+	
+	public void confirmExit() {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Quit Game");
+		alert.setHeaderText("Are you sure?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+
+		if (result.get() == ButtonType.OK){
+			Platform.exit(); // exit application
+		} else  {
+			//do nothing..
+		}
 	}
 
 
