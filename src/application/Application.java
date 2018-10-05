e9cc28a66b6b7d10baf470cf276a8cd43f11dd46
Sam <samong0105@gmail.com>
2018-10-06 12:21:32 +1300

    Added some comments to the Game class and renamed variables to fit Java style guidelines

--- src/application/Application.java
+++ src/application/Application.java
@@ -6,14 +6,11 @@ import persistence.XMLReader;
 
 public class Application {
 
-
-
     public static void main(String[] args){
         Game game = XMLParser.parseGame();
-//        if(game!=null){
-//            game.startGame();
-//        }
-
+        if(game!=null){
+            game.startGame();
+        }
         XMLParser.saveGame(game);
     }
 }
