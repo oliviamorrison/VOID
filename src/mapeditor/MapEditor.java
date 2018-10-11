394675485ed9242bf539285b6dfbccdb0ab2342d
oliviamorrison <olivia.elmorrison@gmail.com>
2018-10-11 15:17:07 +1300

    added items, walls, doors to map editor

--- src/mapeditor/MapEditor.java
+++ src/mapeditor/MapEditor.java
@@ -35,6 +35,8 @@ public class MapEditor extends Application {
         boardGrid.setPrefSize(BOARD_SIZE, BOARD_SIZE);
 
         itemGrid = initItem();
+        initaliseItems();
+
         itemGrid.setStyle("-fx-background-color: lightpink");
 
         // Use a StackPane to display the Image and the Grid
@@ -75,13 +77,6 @@ public class MapEditor extends Application {
             }
         }
 
-        Node n = getNodeByRowColumnIndex(0,0,items);
-
-        if(n instanceof ItemSpace){
-            ItemSpace i = (ItemSpace) n;
-            i.setItem(new Item("green-key.png",new Image(getClass().getResourceAsStream("green-key.png"),100, 100, false, false)));
-        }
-
         items = setOptions(items);
         // Return the GridPane
         return items;
@@ -99,7 +94,7 @@ public class MapEditor extends Application {
                     ItemSpace itemSpace = findFirstEmptyItem();
                     //swap items
                     String name = i.getImageName();
-                    itemSpace.setItem(new Item(name, new Image(getClass().getResourceAsStream("green-key.png"),100, 100, false, false)));
+                    itemSpace.setItem(new Item(name, new Image(getClass().getResourceAsStream(name),100, 100, false, false)));
                     selectedTile.setItem(null);
                     selectedTile.resetImageView();
                 }
@@ -115,7 +110,7 @@ public class MapEditor extends Application {
                     if(selectedTile.getItem()==null) {
                         //swap items
                         String name = i.getImageName();
-                        selectedTile.setItem(new Item(name, new Image(getClass().getResourceAsStream("green-key.png"),20, 20, false, false)));
+                        selectedTile.setItem(new Item(name, new Image(getClass().getResourceAsStream(name),20, 20, false, false)));
                         selectedItem.setItem(null);
                         selectedItem.resetImageView();
                     }
@@ -137,22 +132,129 @@ public class MapEditor extends Application {
         for (int i = 0; i < roomNum; i++) {
             for (int j = 0; j < roomNum; j++) {
 
-                GridPane room = initRoom();
+                //null rooms
+                if(i==0&&j==2 || i==1&&j==0){
+                    board.add(initNullRoom(),j,i);
+                }
+                else {
+                    GridPane room = initRoom(i,j);
 
-                room.setStyle("-fx-padding: 0;" +
-                        "-fx-border-style: solid inside;" +
-                        "-fx-border-width: 0.5;" +
-                        "-fx-border-color: black;");
+                    room.setStyle("-fx-padding: 0;" +
+                            "-fx-border-style: solid inside;" +
+                            "-fx-border-width: 0.5;" +
+                            "-fx-border-color: black;");
 
-                // Add node on j column and i row
-                board.add(room, j, i);
+                    // Add node on j column and i row
+                    board.add(room, j, i);
+                }
             }
         }
+
         // Return the GridPane
         return board;
     }
 
-    private GridPane initRoom() {
+
+    //hard coded items
+    public void initaliseItems(){
+
+        //room 1
+        Node node = getNodeByRowColumnIndex(0,0, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room1 = (GridPane) node;
+            node = getNodeByRowColumnIndex(1, 1, room1);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("player.png",new Image(getClass().getResourceAsStream("player.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 2
+        node = getNodeByRowColumnIndex(0,1, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room2 = (GridPane) node;
+            node = getNodeByRowColumnIndex(9, 5, room2);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("unlit-bomb.png",new Image(getClass().getResourceAsStream("unlit-bomb.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 5
+        node = getNodeByRowColumnIndex(1,1, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room5 = (GridPane) node;
+            node = getNodeByRowColumnIndex(7, 2, room5);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("cutters.png",new Image(getClass().getResourceAsStream("cutters.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 6
+        node = getNodeByRowColumnIndex(1,2, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room6 = (GridPane) node;
+            node = getNodeByRowColumnIndex(2, 7, room6);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("vending-machine.png",new Image(getClass().getResourceAsStream("vending-machine.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 7
+        node = getNodeByRowColumnIndex(2,0, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room7 = (GridPane) node;
+            node = getNodeByRowColumnIndex(5, 5, room7);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("antidote.png",new Image(getClass().getResourceAsStream("antidote.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 8
+        node = getNodeByRowColumnIndex(2,1, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room8 = (GridPane) node;
+            node = getNodeByRowColumnIndex(5,0, room8);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("guard.png",new Image(getClass().getResourceAsStream("guard.png"),17, 17, false, false)));
+            }
+        }
+
+        //room 9
+        node = getNodeByRowColumnIndex(2,2, boardGrid);
+        if(node instanceof GridPane) {
+            GridPane room9 = (GridPane) node;
+            node = getNodeByRowColumnIndex(5, 5, room9);
+
+            if(node instanceof Tile){
+                Tile i = (Tile) node;
+                i.setItem(new Item("two-coins.png",new Image(getClass().getResourceAsStream("two-coins.png"),17, 17, false, false)));
+            }
+        }
+
+    }
+
+    private GridPane initNullRoom(){
+        GridPane room = new GridPane();
+        int roomNum = 3;
+        double roomWidth = BOARD_SIZE / roomNum;
+        double roomHeight = BOARD_SIZE / roomNum;
+
+        room.setStyle("-fx-background-color: gray");
+        return room;
+    }
+
+    private GridPane initRoom(int row, int col) {
         GridPane room = new GridPane();
 
 
@@ -166,19 +268,50 @@ public class MapEditor extends Application {
 
         for (int i = 0; i < tileNum; i++) {
             for (int j = 0; j < tileNum; j++) {
-                MapEditor.Tile tile = new MapEditor.Tile(i, j);
+
+                boolean accessible = true;
+                boolean door = false;
+
+                if(i==0||j==0||i==9||j==9){
+                    accessible = false;
+                    if(j==5 || i==5){
+
+                        //hard coded null rooms
+                        if(row==2 && col ==0 && i==0|| row==0 && col ==0 && i==9 || col==1 && row==1 && j==0)  door = false;
+                        else if(row==0 && col == 1 && j==9 || row==1 && col==2 && i==0) door =false;
+
+                        else if(row==0 && i==0 || row==2 && i==9) door = false;
+                        else if(col==0 && j==0 || col==2 && j==9) door = false;
+
+                        else door = true;
+                    }
+                }
+
+                MapEditor.Tile tile = new MapEditor.Tile(i, j, accessible);
                 // Set each 'Tile' the width and height
                 tile.setPrefSize(tileWidth, tileHeight);
 
-                tile.setStyle("-fx-padding: 0;" +
-                        "-fx-border-style: solid inside;" +
-                        "-fx-border-width: 0.5;" +
-                        "-fx-border-color: black;");
-
+                if(accessible) {
+                    tile.setStyle("-fx-padding: 0;" +
+                            "-fx-border-style: solid inside;" +
+                            "-fx-border-width: 0.5;" +
+                            "-fx-border-color: black;");
+                } else if(door){
+                    tile.setStyle("-fx-padding: 0;" +
+                            "-fx-background-color: white");
+                }
+                //wall
+                else {
+                    tile.setStyle("-fx-padding: 0;" +
+                            "-fx-background-color: lightgray");
+                }
                 // Add node on j column and i row
                 room.add(tile, j, i);
             }
         }
+
+
+
         // Return the GridPane
         return room;
     }
@@ -215,26 +348,34 @@ public class MapEditor extends Application {
         private int positionY;
         private Item item;
         private ImageView imageView;
+        private boolean accessible;
 
-        public Tile(int x, int y) {
+        public Tile(int x, int y, boolean a) {
             positionX = x;
             positionY = y;
             imageView = new ImageView();
-            setOnMouseClicked(e -> {
-                if(selectedTile!=null) {
-                    selectedTile.setStyle("-fx-padding: 0;" +
-                            "-fx-border-style: solid inside;" +
-                            "-fx-border-width: 0.5;" +
-                            "-fx-border-color: black;");
-                }
+            accessible = a;
 
-                selectedTile = this;
-                this.setStyle("-fx-padding: 0;" +
-                        "-fx-border-style: solid inside;" +
-                        "-fx-border-width: 2;" +
-                        "-fx-border-color: red;");
-                System.out.println(positionX + " " + positionY);
-            });
+            if(accessible) {
+                setOnMouseClicked(e -> {
+                    if (selectedTile != null) {
+                        selectedTile.setStyle("-fx-padding: 0;" +
+                                "-fx-border-style: solid inside;" +
+                                "-fx-border-width: 0.5;" +
+                                "-fx-border-color: black;");
+                    }
+
+                    selectedTile = this;
+                    this.setStyle("-fx-padding: 0;" +
+                            "-fx-border-style: solid inside;" +
+                            "-fx-border-width: 2;" +
+                            "-fx-border-color: red;");
+                });
+            }
+        }
+
+        public boolean getAccessible(){
+            return accessible;
         }
 
         public void resetImageView(){
@@ -251,7 +392,6 @@ public class MapEditor extends Application {
 
             if(item!=null) {
                 imageView = new ImageView(item.getImage());
-
                 this.getChildren().add(imageView);
             }
         }
@@ -280,7 +420,6 @@ public class MapEditor extends Application {
                         "-fx-border-style: solid inside;" +
                         "-fx-border-width: 2;" +
                         "-fx-border-color: red;");
-                System.out.println(positionX + " " + positionY);
             });
         }
 
