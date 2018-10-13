0c72b026c37fa9a1fa8f08757215ce847a1f5390
oliviamorrison <olivia.elmorrison@gmail.com>
2018-10-14 11:15:55 +1300

    added map editor tests

--- src/mapeditor/MapEditor.java
+++ src/mapeditor/MapEditor.java
@@ -15,8 +15,6 @@ import javafx.stage.Stage;
 import persistence.XMLParser;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapEditor extends Application {
 
@@ -33,31 +31,44 @@ public class MapEditor extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    // Initialize the grid
-    boardGrid = initBoard();
 
-    // Set the dimensions of the grid
-    boardGrid.setPrefSize(BOARD_SIZE, BOARD_SIZE);
+    setUp();
 
-    itemGrid = initItem();
+    itemGrid = initItemSpaces();
     itemGrid.setStyle("-fx-background-color: lightblue");
-    initaliseItems();
 
-        itemGrid.setStyle("-fx-background-color: lightpink");
+    initaliseItems();
 
     // Use a StackPane to display the Image and the Grid
     mainPane = new GridPane();
     mainPane.add(boardGrid, 0,0);
     mainPane.add(itemGrid,1,0);
 
-
     primaryStage.setScene(new Scene(mainPane));
     primaryStage.setResizable(false);
     primaryStage.show();
   }
 
+  public GridPane getBoardGrid() {
+    return boardGrid;
+  }
 
-  private GridPane initItem(){
+  public GridPane getItemGrid() {
+    return itemGrid;
+  }
+
+  public void setUp(){
+    // Initialize the grid
+    boardGrid = initBoard();
+
+    // Set the dimensions of the grid
+    boardGrid.setPrefSize(BOARD_SIZE, BOARD_SIZE);
+
+
+  }
+
+
+  public GridPane initItemSpaces(){
     GridPane items = new GridPane();
 
     int rows = 2;
---
+++ src/tests/MapEditorTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import javafx.scene.layout.GridPane;
+import mapeditor.MapEditor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MapEditorTests {
+
+    private GridPane board;
+    private GridPane itemGrid;
+    private MapEditor mapEditor;
+
+    public void setUp() {
+        mapEditor = new MapEditor();
+        mapEditor.setUp();
+
+        //board and item grid should be initialized
+        board = mapEditor.getBoardGrid();
+        itemGrid = mapEditor.getItemGrid();
+
+    }
+
+    @Test
+    public void testBoardSetUp(){
+        setUp();
+        assertEquals(mapEditor.getNodeByRowColumnIndex(0,0,board), new GridPane());
+    }
+
+}
