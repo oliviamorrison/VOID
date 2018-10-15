5c525ca18fbb859f01329b00406bbaea640a3510
oliviamorrison <olivia.elmorrison@gmail.com>
2018-10-16 00:39:14 +1300

    added tests

--- src/mapeditor/MapEditorTests.java
+++ src/mapeditor/MapEditorTests.java
@@ -1,11 +1,16 @@
 package mapeditor;
 
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import mapeditor.MapEditor.ItemSpace;
+import mapeditor.MapEditor.TilePane;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class MapEditorTests {
@@ -41,5 +46,87 @@ public class MapEditorTests {
        assertEquals(img, i.getImage());
 
     }
+    
+    @Test
+    public void testAntidoteRoom() {
+    		MapEditor mapEditor = new MapEditor();
+        mapEditor.setUp();
+        
+        TilePane tile = null;
+        
+        //put antidote in room
+        Node node = mapEditor.getNodeByRowColumnIndex(0,0, mapEditor.getBoardGrid());
+        assertTrue(node instanceof GridPane);
+        GridPane room1 = (GridPane) node;
+        node = mapEditor.getNodeByRowColumnIndex(1, 1, room1);
 
+        assertTrue(node instanceof TilePane);
+        tile = (TilePane) node;
+        tile.setMapItem(new MapItem("antidote.png",
+                   new Image(getClass().getResourceAsStream("antidote.png"),
+                           17, 17, false, false)));
+          
+        
+        assertNotNull(tile);
+        assertTrue(mapEditor.isInAntidoteRoom(tile));
+        
+        
+    }
+    
+    @Test
+    public void testInitaliseItems() {
+    		MapEditor mapEditor = new MapEditor();
+        mapEditor.setUp();
+        mapEditor.initaliseItems();
+        
+        TilePane tile = null;
+        
+        Node node = mapEditor.getNodeByRowColumnIndex(0,0, mapEditor.getBoardGrid());
+        assertTrue(node instanceof GridPane);
+        GridPane room1 = (GridPane) node;
+        node = mapEditor.getNodeByRowColumnIndex(1, 1, room1);
+
+        assertTrue(node instanceof TilePane);
+        tile = (TilePane) node;
+        assertEquals(tile.getMapItem().getImageName(),"player.png");
+        
+    }
+    
+    @Test
+    public void createGameXML() {
+    		MapEditor mapEditor = new MapEditor();
+        mapEditor.setUp();
+        mapEditor.initaliseItems();
+        assertTrue(mapEditor.createGame());
+        
+    }
+    
+//    @Test
+//    public void testSetMapItem() {
+//    		MapEditor mapEditor = new MapEditor();
+//        mapEditor.setUp();
+//        mapEditor.initaliseItems();
+//        
+//        ItemSpace tile = null;
+//        
+//        Node node = mapEditor.getNodeByRowColumnIndex(0,0, mapEditor.getO());
+//        assertTrue(node instanceof GridPane);
+//        GridPane room1 = (GridPane) node;
+//        node = mapEditor.getNodeByRowColumnIndex(1, 1, room1);
+//
+//        assertTrue(node instanceof ItemSpace);
+//        tile = (ItemSpace) node;
+//        
+//    }
+    	  @Test
+    	  public void testFindFirstEmptyTile() {
+    		  MapEditor mapEditor = new MapEditor();
+    	      mapEditor.setUp();
+    	      
+    	      Node node = mapEditor.getNodeByRowColumnIndex(0,0, mapEditor.getBoardGrid());
+          assertTrue(node instanceof GridPane);
+          GridPane room1 = (GridPane) node;
+    	      
+    	      assertNotNull(mapEditor.findFirstEmptyTile(room1));
+    	  }
 }
