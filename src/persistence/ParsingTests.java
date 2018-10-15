1d7075a242efcf52ac0c2f130a22a5c6ce965111
Sam <samong0105@gmail.com>
2018-10-16 12:17:59 +1300

    Fixed XmlParser test checkstyle

--- src/persistence/ParsingTests.java
+++ src/persistence/ParsingTests.java
@@ -1,30 +1,42 @@
 package persistence;
 
-import gameworld.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import gameworld.AccessibleTile;
+import gameworld.Alien;
+import gameworld.Bomb;
+import gameworld.Diffuser;
+import gameworld.Direction;
+import gameworld.Game;
+import gameworld.Player;
+import gameworld.Portal;
+import gameworld.Room;
+import gameworld.VendingMachine;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * These tests use JUnit 5 to test the persistence package of the game
+/**These tests use JUnit 5 to test the persistence package of the game.
  *
  * @author Sam Ong 300363819
  */
 public class ParsingTests {
 
-  /**
-   * Test reading a valid XML file into a game
+  /**Test reading a valid XML file into a game.
    * @throws XmlParser.ParseError if it is an invalid file
    */
   @Test
@@ -33,38 +45,36 @@ public class ParsingTests {
     assertNotNull(game);
   }
 
-  /**
-   * Test the parser throws an XmlParser error when an invalid file is loaded
+  /** Test the parser throws an XmlParser error when an invalid file is loaded.
    */
   @Test
   public void testIncorrectSchema() {
     assertThrows(XmlParser.ParseError.class,
-        ()-> XmlParser.parseGame(new File("data/incorrectSchema.xml")));
+        () -> XmlParser.parseGame(new File("data/incorrectSchema.xml")));
   }
 
-  /**
-   * Test the parser throws an XmlParser error when a file with incorrect item names are loaded
+  /**Test the parser throws an XmlParser error when a file with incorrect item names are loaded.
    */
   @Test
   public void testIncorrectItemName() {
     assertThrows(XmlParser.ParseError.class,
-        ()-> XmlParser.parseGame(new File("data/incorrectItemName.xml")));
+        () -> XmlParser.parseGame(new File("data/incorrectItemName.xml")));
   }
 
-  /**
-   * Test the parser throws an XmlParser error when a file with incorrect challenge names are loaded
+  /** Test the parser throws an XmlParser error when a file with incorrect challenge
+   * names are loaded.
    */
   @Test
   public void testIncorrectChallengeName() {
     assertThrows(XmlParser.ParseError.class,
-        ()-> XmlParser.parseGame(new File("data/incorrectChallengeName.xml")));
+        () -> XmlParser.parseGame(new File("data/incorrectChallengeName.xml")));
   }
 
-  /**
-   * Test the parser saves an XML file from a game that starts with a root node <game></game>
+  /** Test the parser saves an XML file from a game that starts with a root node <game></game>.
    */
   @Test
-  public void testSaveFile() throws TransformerException, ParserConfigurationException, IOException, SAXException {
+  public void testSaveFile()
+      throws TransformerException, ParserConfigurationException, IOException, SAXException {
     //Hard coding a room to test saving
     Room[][] board = new Room[3][3];
     for (int i = 0; i < board.length; i++) {
@@ -72,13 +82,13 @@ public class ParsingTests {
         board[i][j] = new Room(i,j);
       }
     }
-
-    Player player = new Player(board[2][2], (AccessibleTile) board[2][2].getTile(5, 5), 100, "NORTH");
     board[0][0].setTile(new Portal(5, 9, board[0][1], Direction.NORTH), 5, 9);
     ((AccessibleTile) board[0][0].getTile(6, 5)).setItem(new Diffuser(6, 5, "NORTH"));
     ((AccessibleTile) board[0][1].getTile(4, 7)).setChallenge(new VendingMachine(4,7, "NORTH"));
     ((AccessibleTile) board[1][0].getTile(2, 2)).setChallenge(new Alien(2,2, "NORTH"));
     ((AccessibleTile) board[2][0].getTile(6, 3)).setChallenge(new Bomb(6,3, "NORTH"));
+    AccessibleTile playerTile = (AccessibleTile) board[2][2].getTile(5, 5);
+    Player player = new Player(board[2][2], playerTile, 100, "NORTH");
 
     player.addItem(new Diffuser(-1,-1, "NORTH"));
 
@@ -91,8 +101,8 @@ public class ParsingTests {
 
     //Check the first element in the file is
     DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    Document doc = dBuilder.parse("data/testSave.xml");
+    DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+    Document doc = dbBuilder.parse("data/testSave.xml");
     NodeList children = doc.getChildNodes();
     Element gameElement = (Element) children.item(0);
     assertEquals(gameElement.getTagName(), "game");
