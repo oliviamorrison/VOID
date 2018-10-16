bf3f572a0c5cf0291a3fec25343dd170fd170944
oliviamorrison <olivia.elmorrison@gmail.com>
2018-10-16 23:28:41 +1300

    more java docs

--- src/mapeditor/MapItem.java
+++ src/mapeditor/MapItem.java
@@ -2,19 +2,37 @@ package mapeditor;
 
 import javafx.scene.image.Image;
 
+/**
+ * This class is a MapItem which is represented by an image
+ * and an image name.
+ */
 public class MapItem {
   private Image image;
   private String imageName;
 
+  /**
+   * Map item constructor which sets the two fields.
+   *
+   * @param n given image name
+   * @param i given image
+   */
   public MapItem(String n, Image i) {
     imageName = n;
     image = i;
   }
 
+  /**
+   * A method to get the image field.
+   * @return image field
+   */
   public Image getImage() {
     return image;
   }
 
+  /**
+   * A method to get the image name.
+   * @return image name
+   */
   public String getImageName() {
     return imageName;
   }
