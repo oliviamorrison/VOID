c37e76d63a59a5e944044b3dd953e18044eb0ab5
James Del Puerto <delpujame@ecs.vuw.ac.nz>
2018-10-16 13:26:22 +1300

    update

--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -15,7 +15,7 @@ public class Renderer {
     private final static Color ACCESSIBLE_COLOUR = Color.rgb(120, 120, 120);
     private final static Color DTColor = Color.rgb(161, 176, 201);
     private final static double floorHeight = 0;
-    private final static double wallHeight = 2;
+    private final static double wallHeight = 0;
     private final static double doorHeight = 0;
 
     private final static double playerHeight = 60;
@@ -97,11 +97,11 @@ public class Renderer {
             AccessibleTile AT = (AccessibleTile) tile;
             color = ACCESSIBLE_COLOUR;
             height = floorHeight;
-            if(AT.hasItem()){
-                color = Color.BLUE;
-            } else if(AT.hasChallenge()){
-                color = Color.RED;
-            }
+//            if(AT.hasItem()){
+//                color = Color.BLUE;
+//            } else if(AT.hasChallenge()){
+//                color = Color.RED;
+//            }
         } else if (tile instanceof InaccessibleTile){
             height = wallHeight;
             color = INACCESSIBLE_COLOUR;
