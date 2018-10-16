f728ffa17075786e0ebdc0726582edf2bb83bc09
latrellwhata <latrellwhata@outlook.com>
2018-10-16 23:54:43 +1300

    Last second commit

---
+++ src/Tests.java
@@ -0,0 +1,10 @@
+/**
+ * This class should have our tests.
+ */
+public class Tests {
+
+  // our team missed the specification which stated the tests must be included in this file
+  // test coverage for the project can be checked by running test coverage on the src file
+  // individual test classes are located in each package
+
+}
